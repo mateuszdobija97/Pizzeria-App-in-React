0a8b26c6e3a9bb698e6637d8c26116934bbf8954
Mateusz Dobija <mateusz.dobija.rce@gmail.com>
2020-05-06 18:16:34 +0200

    Updates

---
+++ precache-manifest.7df7da9d4f4ceca0ef9d270de2cccb41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c1d0879d559b9c0d427d8a68889b5d",
+    "url": "/Pizzeria-App-in-React/index.html"
+  },
+  {
+    "revision": "8c7d0b5a0fd61a2e4681",
+    "url": "/Pizzeria-App-in-React/static/css/main.8df70aee.chunk.css"
+  },
+  {
+    "revision": "87a15ab40bcee11ff1c6",
+    "url": "/Pizzeria-App-in-React/static/js/2.7e9c049e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pizzeria-App-in-React/static/js/2.7e9c049e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7d0b5a0fd61a2e4681",
+    "url": "/Pizzeria-App-in-React/static/js/main.4c5f0357.chunk.js"
+  },
+  {
+    "revision": "39516ecffcd1e9b9edd9",
+    "url": "/Pizzeria-App-in-React/static/js/runtime-main.c033a0ca.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/adres%20strony/precache-manifest.f897dfb20b0d359815938b6f5d3f3210.js"
+  "/Pizzeria-App-in-React/precache-manifest.7df7da9d4f4ceca0ef9d270de2cccb41.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adres%20strony/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Pizzeria-App-in-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
